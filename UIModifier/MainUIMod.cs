a29d00ce361840b46394f325f0886d5f5c7f78b1
Crashaholic <33598082+Crashaholic@users.noreply.github.com>
2020-03-07 21:34:24 +0800

    About to add a new custom canvas. Started with Exp bar but doesn't seem to work

--- UIModifier/MainUIMod.cs
+++ UIModifier/MainUIMod.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using BepInEx;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using R2API;
-using RoR2.UI;
-using RoR2;
-using On.RoR2.UI;
 
 /*
 	Disclaimer:
 	Despite it all
 	I have no idea what I'm doing to be very honest.
 */
+
+#region TODO:
+	/*
+	 
+	*/
+#endregion
 
 namespace UIModifier
 {
@@ -22,16 +22,21 @@ namespace UIModifier
 	[BepInPlugin("com.ohway.UIMod", "UI Modifier", "1.0")]
 	public class MainUIMod : BaseUnityPlugin
 	{
-		private GameObject ModHealthBarReference = null;
-		private GameObject ModHealthBarDecoration = null;
-		private GameObject ModShrunkenRoot = null;
-		private GameObject ModSlashText = null;
-		private GameObject MainHPBarRef = null;
-		private RectTransform[] rectTransforms = null;
-		private string assetPrefix = "@ohwayUIMod";
-		private string bundleName = "textures";
-		private string Path_CircularMask = "Assets/textures/circularMask.png";
-		private string Path_HealthGlobeDecoration = "Assets/textures/hpOverlay.png";
+		public GameObject ModCanvas = null;
+
+		#region Health Bar GameObjects
+		// If you're looking for Exp Bar GameObjects, look downwards
+		private GameObject ModHealthBarRoot = null;
+		public GameObject ModHealthBarReference = null;
+		public GameObject ModHealthBarDecoration = null;
+		public GameObject ModHealthShrunkenRoot = null;
+		public GameObject ModHealthSlashText = null;
+		public GameObject VanillaHPBarRef = null;
+		#endregion
+		public string assetPrefix = "@ohwayUIMod";
+		public string bundleName = "textures";
+		public string Path_CircularMask = "Assets/textures/circularMask.png";
+		public string Path_HealthGlobeDecoration = "Assets/textures/hpOverlay.png";
 
 		private string ResPath(string assetPath) => assetPrefix + ':' + assetPath;
 
@@ -44,39 +49,37 @@ namespace UIModifier
 				R2API.ResourcesAPI.AddProvider(provider);
 			}
 			On.RoR2.UI.HealthBar.Awake += HealthBarAwakeAddon;
+			On.RoR2.UI.ExpBar.Awake += ExpBarAwakeAddon;
 		}
 
 		private void HealthBarAwakeAddon(On.RoR2.UI.HealthBar.orig_Awake orig, RoR2.UI.HealthBar self)
 		{
 			orig(self);
 			var currentRect = self.gameObject.GetComponentsInChildren<RectTransform>();
-			if (currentRect != null && MainHPBarRef == null)
+			if (currentRect != null && VanillaHPBarRef == null)
 			{
-				rectTransforms = currentRect;
-				for (int i = 0; i < rectTransforms.Length; ++i)
+				for (int i = 0; i < currentRect.Length; ++i)
 				{
-					if (rectTransforms[i].name == "HealthbarRoot")
+					if (currentRect[i].name == "HealthbarRoot")
 					{
-						MainHPBarRef = rectTransforms[i].gameObject;
-						MainHPOrbStart();
+						VanillaHPBarRef = currentRect[i].gameObject;
+						MainHPGlobeStart();
 					}
 				}
 			}
 		}
 
-		void OnGUI()
+		private void MainHPGlobeStart()
 		{
-			//var strings = Assembly.GetExecutingAssembly().GetManifestResourceNames();
-			//for (int i = 0; i < strings.Length; i++)
-			//{
-			//	GUILayout.Label(strings[i]);
-			//}
-		}
-
-		private void MainHPOrbStart()
-		{
-			if (MainHPBarRef != null)
+			if (VanillaHPBarRef != null)
 			{
+				ModCanvas = Instantiate(new GameObject("UIModifierCanvas"));
+				ModCanvas.AddComponent<Canvas>();
+				ModCanvas.AddComponent<CanvasScaler>();
+				ModCanvas.AddComponent<GraphicRaycaster>();
+
+				Destroy(GameObject.Find("BarRoots").GetComponent<Image>());
+				ModHealthBarRoot = Instantiate(new GameObject("HealthGlobeRoot"));
 				ModHealthBarReference = Instantiate(new GameObject("HealthGlobe"));
 				ModHealthBarDecoration = Instantiate(new GameObject("HealthGlobeBacking"));
 
@@ -90,24 +93,31 @@ namespace UIModifier
 				ModHealthBarDecoration.AddComponent<Image>();
 				ModHealthBarDecoration.GetComponent<Image>().sprite = Resources.Load<Sprite>(ResPath(Path_HealthGlobeDecoration));
 
-				Destroy(MainHPBarRef.GetComponent<Image>());
-				ModShrunkenRoot = MainHPBarRef.transform.GetChild(0).gameObject;
-				ModSlashText = MainHPBarRef.transform.GetChild(1).gameObject;
+				ModHealthBarRoot.AddComponent<RectTransform>();
 
-				ModHealthBarReference.transform.position = ModShrunkenRoot.transform.position;
-				ModHealthBarReference.transform.SetParent(ModShrunkenRoot.transform.root);
+				Destroy(VanillaHPBarRef.GetComponent<Image>());
+				ModHealthShrunkenRoot = VanillaHPBarRef.transform.GetChild(0).gameObject;
+				ModHealthSlashText = VanillaHPBarRef.transform.GetChild(1).gameObject;
+
+				ModHealthBarRoot.transform.SetParent(ModHealthShrunkenRoot.transform.root);
+				ModHealthBarReference.transform.position = ModHealthShrunkenRoot.transform.position;
+				ModHealthBarReference.transform.SetParent(ModHealthBarRoot.transform.root);
 				ModHealthBarDecoration.transform.position = ModHealthBarReference.transform.position;
-				ModHealthBarDecoration.transform.SetParent(ModShrunkenRoot.transform.root);
-				ModShrunkenRoot.transform.SetParent(ModHealthBarReference.transform);
-				ModSlashText.transform.SetParent(ModHealthBarReference.transform.parent);
+				ModHealthBarDecoration.transform.SetParent(ModHealthBarRoot.transform.root);
+				ModHealthShrunkenRoot.transform.SetParent(ModHealthBarReference.transform);
+				ModHealthSlashText.transform.SetParent(ModHealthBarReference.transform.parent);
 
-				ModShrunkenRoot.GetComponent<RectTransform>().anchorMin = Vector2.zero;
-				ModShrunkenRoot.GetComponent<RectTransform>().anchorMax = Vector2.one;
-				ModShrunkenRoot.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 100);
+				ModHealthShrunkenRoot.GetComponent<RectTransform>().anchorMin = Vector2.zero;
+				ModHealthShrunkenRoot.GetComponent<RectTransform>().anchorMax = Vector2.one;
+				ModHealthShrunkenRoot.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 100);
+
+				ModHealthBarRoot.GetComponent<RectTransform>().anchorMin = Vector2.zero;
+				ModHealthBarRoot.GetComponent<RectTransform>().anchorMax = Vector2.one;
+				ModHealthBarRoot.GetComponent<RectTransform>().sizeDelta = Vector2.zero;
 
 				ModHealthBarReference.GetComponent<RectTransform>().anchorMin = Vector2.zero;
 				ModHealthBarReference.GetComponent<RectTransform>().anchorMax = Vector2.zero;
-				ModHealthBarReference.GetComponent<RectTransform>().sizeDelta = new Vector2(228, 228);
+				ModHealthBarReference.GetComponent<RectTransform>().sizeDelta = new Vector2(229, 229);
 				ModHealthBarReference.GetComponent<RectTransform>().anchoredPosition = new Vector2(100, 110);
 
 				ModHealthBarDecoration.GetComponent<RectTransform>().anchorMin = Vector2.zero;
@@ -115,14 +125,15 @@ namespace UIModifier
 				ModHealthBarDecoration.GetComponent<RectTransform>().sizeDelta = new Vector2(250, 250);
 				ModHealthBarDecoration.GetComponent<RectTransform>().anchoredPosition = new Vector2(100, 110);
 
-				ModSlashText.GetComponent<RectTransform>().anchorMin = Vector2.zero;
-				ModSlashText.GetComponent<RectTransform>().anchorMax = Vector2.zero;
-				ModSlashText.GetComponent<RectTransform>().pivot = Vector2.zero;
-				ModSlashText.GetComponent<RectTransform>().anchoredPosition = new Vector2(100, 250);
+				ModHealthSlashText.GetComponent<RectTransform>().anchorMin = Vector2.zero;
+				ModHealthSlashText.GetComponent<RectTransform>().anchorMax = Vector2.zero;
+				ModHealthSlashText.GetComponent<RectTransform>().pivot = Vector2.zero;
+				ModHealthSlashText.GetComponent<RectTransform>().anchoredPosition = new Vector2(100, 250);
 
 				ModHealthBarReference.transform.Rotate(0f, 0f, 90f);
 				ModHealthBarDecoration.transform.SetSiblingIndex(1);
 				ModHealthBarReference.transform.SetSiblingIndex(0);
+				ModHealthBarRoot.transform.SetAsFirstSibling();
 			}
 			else
 			{
@@ -130,21 +141,59 @@ namespace UIModifier
 			}
 		}
 
-		void Start()
+		#region Exp bar GameObjects (DOES NOTHING FOR NOW)
+		public GameObject VanillaExpBarRoot = null;
+		public GameObject ModExpShrunkenRoot = null;
+		public GameObject ModExpBarGroup = null;
+		#endregion
+		public void ExpBarAwakeAddon(On.RoR2.UI.ExpBar.orig_Awake orig, RoR2.UI.ExpBar self)
 		{
-
+			orig(self);
+			var currentRect = self.gameObject.GetComponentsInChildren<RectTransform>();
+			if (currentRect != null && VanillaExpBarRoot == null)
+			{
+				for (int i = 0; i < currentRect.Length; ++i)
+				{
+					if (currentRect[i].name == "ExpBarRoot")
+					{
+						//VanillaExpBarRoot = currentRect[i].gameObject;
+						//MainExpBarStart();
+					}
+				}
+			}
 		}
 
-		void Update()
+		private void MainExpBarStart()
 		{
+			ModExpShrunkenRoot = VanillaExpBarRoot.transform.GetChild(0).gameObject;
 
+			ModExpBarGroup = Instantiate(new GameObject("XPBarGroup"));
+			ModExpBarGroup.AddComponent<RectTransform>();
+
+			ModExpBarGroup.transform.SetParent(VanillaExpBarRoot.transform.root);
+			ModExpShrunkenRoot.transform.SetParent(ModExpBarGroup.transform);
+
+			ModExpBarGroup.GetComponent<RectTransform>().anchorMin = new Vector2(0.5f, 0f);
+			ModExpBarGroup.GetComponent<RectTransform>().anchorMax = new Vector2(0.5f, 0f);
+			ModExpBarGroup.GetComponent<RectTransform>().anchoredPosition = new Vector2(0f, 30f);
+
+			ModExpShrunkenRoot.GetComponent<RectTransform>().anchorMin = new Vector2(0.5f, 0f);
+			ModExpShrunkenRoot.GetComponent<RectTransform>().anchorMax = new Vector2(0.5f, 0f);
+			ModExpShrunkenRoot.GetComponent<RectTransform>().sizeDelta = new Vector2(300f, 10f);
+			ModExpShrunkenRoot.GetComponent<RectTransform>().anchoredPosition = new Vector2(0f, 0f);
+		}
+
+		void OnGUI()
+		{
 		}
 
 		void OnDestroy()
 		{
 			On.RoR2.UI.HealthBar.Awake -= HealthBarAwakeAddon;
+			On.RoR2.UI.ExpBar.Awake -= ExpBarAwakeAddon;
 			Destroy(ModHealthBarReference);
 			Destroy(ModHealthBarDecoration);
+			Destroy(ModHealthBarRoot);
 		}
 	}
 }
